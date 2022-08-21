0caea41648d958f17f75b9ccb7fc3fccf30c73ba
Ali Khan <alik@robarts.ca>
2022-08-20 21:46:22 -0400

    initial commit

---
+++ example/run.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+from snakebids.app import SnakeBidsApp
+
+
+def main():
+
+    app = SnakeBidsApp(Path(__file__).resolve().parent.parent)  # to get repository root
+    app.run_snakemake()
+
+
+if __name__ == "__main__":
+    main()
